d70800edae01fe02ddc04a586aa1438831a34e6e
Augustine Francis <enyiaugustinefrancis@gmail.com>
2024-06-21 20:04:52 +0100

    Weekend Homework

---
+++ week2/home_work.py
@@ -0,0 +1,4 @@
+# A simple function that removes the first letter in a word ex. Jason returns ason
+# Write a function that removes the last letter in a word ex. Jason returns Jaso
+# Write a function that removes all vowel sounds in a string ex. Jason returns Jsn
+# Write a function that counts all the vowel sounds in a string ex. Jason retuns 2
